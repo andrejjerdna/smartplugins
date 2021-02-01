5a7f7ab0c0c4438b5fd1e0aa882bd58201ae9c0c
Varvara <stepanovavarvara15@gmail.com>
2021-02-01 10:49:44 +0300

    Start

---
+++ SmartPlugins/Common/SmartTeklaModel/PluginAnalysis/UserRun.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Model;
+
+namespace SmartTeklaModel.PluginAnalysis
+{
+    public class UserRun
+    {
+        private Model _model;
+        public string ID { get; }
+        public System.Net.IPAddress IP { get; }
+        public string TeklaVersion { get; }
+
+        public UserRun(Model model, string id)
+        {
+            _model = model;
+            ID = id;
+
+            IP = GetUserIP();
+        }
+
+       /* private void GetTeklaVersion()
+        {
+            _model.GetInfo().
+        }*/
+
+        private System.Net.IPAddress GetUserIP()
+        {
+            var host = System.Net.Dns.GetHostName();
+
+           return System.Net.Dns.GetHostEntry(host).AddressList[0];            
+        }
+    }
+}
---
+++ SmartPlugins/Common/TestForm/Form1.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using SmartTeklaModel.PluginAnalysis;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var button = new UserRun(new Model(), "testID");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            string output = JsonConvert.SerializeObject(new UserRun(new Model(), "testID"));
+
+        }
+    }
+}
