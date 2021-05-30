09dd2d7381241af690602872ef468d5e0d7bcfbe
Степанов Андрей <33proekta@mail.ru>
2021-05-30 22:41:28 +0300

    Temp

---
+++ SmartPlugins/Common/SmartExtensions/AssemblyExtension.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Model;
+
+namespace SmartExtensions
+{
+    public static class AssemblyExtension
+    {
+        /// <summary>
+        /// Получаем все детали сборки.
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <param name="getMainPart">Надо ли получать главную деталь сборки.</param>
+        /// <returns></returns>
+        public static IEnumerable<Part> GetAllParts(this Assembly assembly, bool getMainPart)
+        {
+            var result = new List<Part>();
+
+            if (getMainPart)
+                result.Add(assembly.GetMainPart() as Part);
+
+            var secondariesParts = assembly.GetSecondaries().OfType<Part>();
+            result.AddRange(secondariesParts);
+
+            return result.Where(part => part != null);
+        }
+
+        /// <summary>
+        /// Получаем всю арматуру сборки сборки.
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <param name="getMainPart">Надо ли получать главную деталь сборки.</param>
+        /// <returns></returns>
+        public static IEnumerable<Part> GetAllRebars(this Assembly assembly, bool getMainPart)
+        {
+            var result = assembly.GetAllParts(true);
+
+            if (getMainPart)
+                result.Add(assembly.GetMainPart() as Part);
+
+            var secondariesParts = assembly.GetSecondaries().OfType<Part>();
+            result.AddRange(secondariesParts);
+
+            return result.Where(part => part != null);
+        }
+    }
+}
---
+++ SmartPlugins/Common/SmartTeklaModel/Rebar/RebarNumberator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Model;
+
+namespace SmartTeklaModel.Rebar
+{
+    public class RebarNumberator
+    {
+        private Assembly _assembly;
+        
+        public RebarNumberator(Assembly assembly)
+        {
+            _assembly = assembly;
+        }
+
+        public void RefreshNumbers()
+        {
+
+        }
+    }
+}
