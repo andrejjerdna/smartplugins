ada951f19b9cc43c7d07bccdf03db5c196a46097
astepanov-sl <astepanov@sodislab.com>
2021-10-14 21:59:42 +0300

    Added SmartPlugins.Macroses.Library

--- SmartPlugins/Commercial/sp_DimensionsForReinforcement/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Windows;
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("64872190-4CB9-4FF3-A898-019D463EB986")]
