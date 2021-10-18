8540c72f60831df0040d4d9371c026e899dd46df
Alexey <theArnkey@live.com>
2021-10-18 23:00:33 +1000

    fix + refactor

--- src/Macroses/SmartPlugins.Macroses.Library/EmbededMark.cs
+++ src/Macroses/SmartPlugins.Macroses.Library/EmbededMark.cs
@@ -1,15 +1,7 @@
 ﻿using SmartPlugins.Common.Abstractions;
 using SmartPlugins.Common.SmartTeklaModel;
-
 using tsd = Tekla.Structures.Drawing;
 using tsg = Tekla.Structures.Geometry3d;
-using Tekla.Structures.Drawing.UI;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SmartPlugins.Macroses.Library
 {
@@ -17,32 +9,29 @@ namespace SmartPlugins.Macroses.Library
     {
         public void Run()
         {
-            var model = new SmartModel();
+            var smartModel = new SmartModel();
 			
-            if (!model.ConnectionStatus)
-                return;
-			// to do smart picker drawing
-			tsd.DrawingHandler dh = new tsd.DrawingHandler();
-			tsd.Drawing draw = dh.GetActiveDrawing();
+            if (!smartModel.ConnectionStatus)
+				return;
 
-			tsd.DrawingObjectEnumerator doe = dh.GetDrawingObjectSelector().GetSelected();
+			var drawingHandler = new tsd.DrawingHandler();
+			var activeDrawing = drawingHandler.GetActiveDrawing();
+			var selectedDrawingObjects = drawingHandler.GetDrawingObjectSelector().GetSelected();
 
-			tsd.UI.Picker picker = dh.GetPicker();
-			tsd.ViewBase vb = null;
-			tsg.Point p = new tsg.Point(0, 0, 0);
-			picker.PickPoint("", out p, out vb);
+			var picker = drawingHandler.GetPicker();
+			tsd.ViewBase viewBase = null;
+			var newPoint = new tsg.Point(0, 0, 0);
+			picker.PickPoint("", out newPoint, out viewBase);
 
-			while (doe.MoveNext())
+			while (selectedDrawingObjects.MoveNext())
 			{
-				if (doe.Current is tsd.Mark)
+				if (selectedDrawingObjects.Current is tsd.Mark)
 				{
-
-					tsd.Mark m = doe.Current as tsd.Mark;
-					m.InsertionPoint = p;
-					m.Modify();
-					m.InsertionPoint = p;
-					m.Modify();
-
+					tsd.Mark mark = selectedDrawingObjects.Current as tsd.Mark;
+					mark.InsertionPoint = newPoint;
+					mark.Modify();
+					mark.InsertionPoint = newPoint;
+					mark.Modify();
 				}
 			}
 		}
