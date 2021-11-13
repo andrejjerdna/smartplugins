abbdf00ec4d7ce188ba73ae82340658477b47742
astepanov-sl <astepanov@sodislab.com>
2021-11-14 00:16:36 +0300

    Added tests concept

---
+++ src/Tests/SmartPlugins.Tests.UnitTests/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SmartPlugins.Tests.UnitTests
+{
+    public abstract class BaseTest
+    {
+        /// <summary>
+        /// Container configure
+        /// </summary>
+        protected TestsContainerConfigure ContainerConfigure;
+
+        /// <summary>
+        /// IoC container
+        /// </summary>
+        protected IContainer Container;
+
+        /// <summary>
+        /// Get IoC container
+        /// </summary>
+        [TestInitialize]
+        public void Init()
+        {
+            ContainerConfigure = TestsContainerConfigure.GetContainer();
+
+            Container = ContainerConfigure.Build();
+        }
+    }
+}
