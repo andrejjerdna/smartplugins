11f48ce82c99bc2fdee9fc6ea72ffdbb57a04b0e
astepanov-sl <astepanov@sodislab.com>
2021-12-18 00:14:17 +0300

    Added AssemblyOperations, TeklaProperties. Refactoring MainPartByWeight.

---
+++ src/Common/SmartPlugins.Common.TeklaLibrary/TeklaProperties.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartPlugins.Common.TeklaLibrary
+{
+    public struct TeklaProperties
+    {
+        /// <summary>
+        /// Shows the weight of the object
+        /// https://support.tekla.com/doc/tekla-structures/2021/weight
+        /// </summary>
+        public const string Weight = "WEIGHT";
+    }
+}
